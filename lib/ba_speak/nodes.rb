e06e72b2ba8559d0a00f6ee5c0bbc37fef77eddd
Mark Maglana <mmaglana@gmail.com>
2012-06-14 01:16:39 +0800

    Implement BA Speak parser

---
+++ lib/ba_speak/nodes.rb
@@ -0,0 +1,23 @@
+module BaSpeak
+
+  class Node
+    attr_reader :children, :name
+
+    def initialize(name='', children=[])
+      @children = children
+      @name     = name
+    end
+
+    def ==(other_node)
+      name == other_node.name && @children == other_node.children
+    end
+  end
+
+  class RootNode < Node; end
+  class GroupNode < Node; end
+  class RequirementNode < Node; end
+  class ExamplesNode < Node; end
+  class RowNode < Node; end
+  class TextNode < Node; end
+
+end
